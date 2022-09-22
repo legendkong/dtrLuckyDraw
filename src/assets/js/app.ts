ed630f34fc9f5a36fece62ddffdfd847e669e9bf
Jonathan Kong Boon Lieng <boonlieng.kong@circles.asia>
2022-09-22 15:24:22 +0800

    feat: update assets and styles

--- src/assets/js/app.ts
+++ src/assets/js/app.ts
@@ -4,46 +4,82 @@ import SoundEffects from '@js/SoundEffects';
 
 // Initialize slot machine
 (() => {
-  const drawButton = document.getElementById('draw-button') as HTMLButtonElement | null;
-  const fullscreenButton = document.getElementById('fullscreen-button') as HTMLButtonElement | null;
-  const settingsButton = document.getElementById('settings-button') as HTMLButtonElement | null;
-  const settingsWrapper = document.getElementById('settings') as HTMLDivElement | null;
-  const settingsContent = document.getElementById('settings-panel') as HTMLDivElement | null;
-  const settingsSaveButton = document.getElementById('settings-save') as HTMLButtonElement | null;
-  const settingsCloseButton = document.getElementById('settings-close') as HTMLButtonElement | null;
-  const sunburstSvg = document.getElementById('sunburst') as HTMLImageElement | null;
-  const confettiCanvas = document.getElementById('confetti-canvas') as HTMLCanvasElement | null;
-  const nameListTextArea = document.getElementById('name-list') as HTMLTextAreaElement | null;
-  const removeNameFromListCheckbox = document.getElementById('remove-from-list') as HTMLInputElement | null;
-  const enableSoundCheckbox = document.getElementById('enable-sound') as HTMLInputElement | null;
+  const drawButton = document.getElementById(
+    'draw-button'
+  ) as HTMLButtonElement | null;
+  const fullscreenButton = document.getElementById(
+    'fullscreen-button'
+  ) as HTMLButtonElement | null;
+  const settingsButton = document.getElementById(
+    'settings-button'
+  ) as HTMLButtonElement | null;
+  const settingsWrapper = document.getElementById(
+    'settings'
+  ) as HTMLDivElement | null;
+  const settingsContent = document.getElementById(
+    'settings-panel'
+  ) as HTMLDivElement | null;
+  const settingsSaveButton = document.getElementById(
+    'settings-save'
+  ) as HTMLButtonElement | null;
+  const settingsCloseButton = document.getElementById(
+    'settings-close'
+  ) as HTMLButtonElement | null;
+  const sunburstSvg = document.getElementById(
+    'sunburst'
+  ) as HTMLImageElement | null;
+  const confettiCanvas = document.getElementById(
+    'confetti-canvas'
+  ) as HTMLCanvasElement | null;
+  const nameListTextArea = document.getElementById(
+    'name-list'
+  ) as HTMLTextAreaElement | null;
+  const removeNameFromListCheckbox = document.getElementById(
+    'remove-from-list'
+  ) as HTMLInputElement | null;
+  const enableSoundCheckbox = document.getElementById(
+    'enable-sound'
+  ) as HTMLInputElement | null;
 
   // Graceful exit if necessary elements are not found
-  if (!(
-    drawButton
-    && fullscreenButton
-    && settingsButton
-    && settingsWrapper
-    && settingsContent
-    && settingsSaveButton
-    && settingsCloseButton
-    && sunburstSvg
-    && confettiCanvas
-    && nameListTextArea
-    && removeNameFromListCheckbox
-    && enableSoundCheckbox
-  )) {
+  if (
+    !(
+      drawButton
+      && fullscreenButton
+      && settingsButton
+      && settingsWrapper
+      && settingsContent
+      && settingsSaveButton
+      && settingsCloseButton
+      && sunburstSvg
+      && confettiCanvas
+      && nameListTextArea
+      && removeNameFromListCheckbox
+      && enableSoundCheckbox
+    )
+  ) {
     console.error('One or more Element ID is invalid. This is possibly a bug.');
     return;
   }
 
   if (!(confettiCanvas instanceof HTMLCanvasElement)) {
-    console.error('Confetti canvas is not an instance of Canvas. This is possibly a bug.');
+    console.error(
+      'Confetti canvas is not an instance of Canvas. This is possibly a bug.'
+    );
     return;
   }
 
   const soundEffects = new SoundEffects();
   const MAX_REEL_ITEMS = 40;
-  const CONFETTI_COLORS = ['#26ccff', '#a25afd', '#ff5e7e', '#88ff5a', '#fcff42', '#ffa62d', '#ff36ff'];
+  const CONFETTI_COLORS = [
+    '#26ccff',
+    '#a25afd',
+    '#ff5e7e',
+    '#88ff5a',
+    '#fcff42',
+    '#ffa62d',
+    '#ff36ff'
+  ];
   let confettiAnimationId;
 
   /** Confeetti animation instance */
@@ -54,7 +90,9 @@ import SoundEffects from '@js/SoundEffects';
 
   /** Triggers cconfeetti animation until animation is canceled */
   const confettiAnimation = () => {
-    const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+    const windowWidth = window.innerWidth
+      || document.documentElement.clientWidth
+      || document.getElementsByTagName('body')[0].clientWidth;
     const confettiScale = Math.max(0.5, Math.min(1, windowWidth / 1100));
 
     customConfetti({
@@ -62,7 +100,9 @@ import SoundEffects from '@js/SoundEffects';
       gravity: 0.8,
       spread: 90,
       origin: { y: 0.6 },
-      colors: [CONFETTI_COLORS[Math.floor(Math.random() * CONFETTI_COLORS.length)]],
+      colors: [
+        CONFETTI_COLORS[Math.floor(Math.random() * CONFETTI_COLORS.length)]
+      ],
       scalar: confettiScale
     });
 
@@ -128,9 +168,9 @@ import SoundEffects from '@js/SoundEffects';
   });
 
   // Hide fullscreen button when it is not supported
-  if (!(document.documentElement.requestFullscreen && document.exitFullscreen)) {
-    fullscreenButton.remove();
-  }
+  // if (!(document.documentElement.requestFullscreen && document.exitFullscreen)) {
+  //   fullscreenButton.remove();
+  // }
 
   // Click handler for "Fullscreen" button
   fullscreenButton.addEventListener('click', () => {
@@ -150,7 +190,9 @@ import SoundEffects from '@js/SoundEffects';
   // Click handler for "Save" button for setting page
   settingsSaveButton.addEventListener('click', () => {
     slot.names = nameListTextArea.value
-      ? nameListTextArea.value.split(/\n/).filter((name) => Boolean(name.trim()))
+      ? nameListTextArea.value
+        .split(/\n/)
+        .filter((name) => Boolean(name.trim()))
       : [];
     slot.shouldRemoveWinnerFromNameList = removeNameFromListCheckbox.checked;
     soundEffects.mute = !enableSoundCheckbox.checked;
